d770e206a55a342511b1c014dcfad3ff065a58da
ppyp3 <ppyp3ss@gmail.com>
2026-09-24 05:04:28 +0300

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -179,7 +179,7 @@ def check_media_size_before_download(url: str, mode: str = "video") -> bool:
     if mode in ["audio", "yt_audio", "yt_voice"]:
         ydl_opts['format'] = 'bestaudio/best'
     else:
-        ydl_opts['format'] = 'bestvideo[max_filesize<=49M][ext=mp4]+bestaudio[ext=m4a]/best[max_filesize<=49M][ext=mp4]/best[max_filesize<=49M]/best'
+        ydl_opts['format'] = 'bv*[filesize<49M]+ba/b[filesize<49M]/best'
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -246,7 +246,7 @@ def download_youtube_media(url: str, mode: str = "video"):
         })
     else:
         ydl_opts.update({
-            'format': 'bestvideo[max_filesize<=49M][ext=mp4]+bestaudio[ext=m4a]/best[max_filesize<=49M][ext=mp4]/best[max_filesize<=49M]/best',
+            'format': 'bv*[filesize<49M]+ba/b[filesize<49M]/best',
         })
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
