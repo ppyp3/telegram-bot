13d33fb7b095ee5481687af7f81b0dd45e8ccb4b
ppyp3 <ppyp3ss@gmail.com>
2026-09-26 02:33:21 +0300

    Simplify format option in youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -195,6 +195,8 @@ def check_media_size_before_download(url: str, mode: str = "video") -> bool:
 
   if mode in ["audio", "yt_audio", "yt_voice"]:
     ydl_opts["format"] = "bestaudio/best"
+  else:
+    ydl_opts["format"] = "best"
 
   try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -265,7 +267,7 @@ def download_youtube_media(url: str, mode: str = "video"):
     })
   else:
     ydl_opts.update({
-        "format": "best/bestvideo+bestaudio/best",
+        "format": "best",
     })
 
   try:
